9f49c5b8bf8418e31cac55fd01c820f00f2c502c
Twinkle <pateltwinkle2096@gmail.com>
2023-04-28 20:13:37 -0400

    print days using switch case

--- FirstconsoleApp/Program.cs
+++ FirstconsoleApp/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.ComponentModel;
+
+int day = 6;
+switch (day){
+    case 0:
+        Console.WriteLine("Sunday");
+        break;    
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+    case 2:
+        Console.WriteLine("Tueday");
+        break;
+    case 3:
+        Console.WriteLine("Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("Thursday");
+        break;
+
+    case 5:
+        Console.WriteLine("Friday");
+        break;
+    case 6:
+        Console.WriteLine("Saturday");
+        break;
+
+}
+
